a32ee9ef43c6433fd2fba6b179e252af9248ce2b
LukeNguyen <luongnguyen408@southseattle.edu>
2019-03-05 15:19:42 -0800

    scrolling backgrounp scrip C++
    adding backgrounp but there is a camera problem

---
+++ First Unity Project/Assets/Luke_Scenes/BGscroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGscroll : MonoBehaviour
+{
+    public float scroll_speech = 0.5f;
+
+    private MeshRenderer mesh_Renderer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Time.time * scroll_speech;
+
+        Vector2 offset = new Vector2(x, 0);
+
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
