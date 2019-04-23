7ac1ccaf77481b02fc532bb744f2c58ba5852ed3
JayL <JayL@users.noreply.github.com>
2019-04-23 00:23:54 -0700

    pausebutton

---
+++ First Unity Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenuLevel;
+
+    public void RestartGame()
+    {
+        FindObjectOfType<GameManager>().Reset();
+    }
+
+    public void QuitToMain()
+    {
+        Application.LoadLevel(mainMenuLevel);
+    }
+}
