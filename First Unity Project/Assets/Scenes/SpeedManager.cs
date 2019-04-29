dc9ddf56c2d021030b2542afa705072dcc8f95fd
JayL <JayL@users.noreply.github.com>
2019-04-29 00:27:17 -0700

    Added a Demo Stage

---
+++ First Unity Project/Assets/Scenes/SpeedManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedManager : MonoBehaviour
+{
+
+    public PlayerController thePlayer;
+    public Text speedText;
+
+    public float speedCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedCount = thePlayer.moveSpeed;
+        speedText.text = "Speed: " + speedCount;
+        
+    }
+}
